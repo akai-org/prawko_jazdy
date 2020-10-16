acf120a556e026c2113f307b3bf673ae436021ee
wojtek <krkezc@gmail.com>
2020-10-16 22:00:22 +0200

    added lock on db singleton

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:floor/floor.dart';
 import 'package:sqflite/sqflite.dart' as sqflite;
+import 'package:synchronized/synchronized.dart';
 
 import 'models/StudentDriverModel.dart';
 import 'student_drivers_dao.dart';
@@ -18,10 +19,13 @@ class StudentDriversDatabase {
   StudentDriversDatabase._();
 
   static AppDatabase _instance;
+  static Lock lock = Lock();
 
   static Future<AppDatabase> get instance async {
-    _instance ??=
-        await $FloorAppDatabase.databaseBuilder('app_database.db').build();
+    lock.synchronized(() async {
+      _instance ??=
+          await $FloorAppDatabase.databaseBuilder('app_database.db').build();
+    });
     return _instance;
   }
 }
